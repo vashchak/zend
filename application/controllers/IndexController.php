088b5b7bc9a367c4300ef7cd902726fa0b6a0654
vashchak <vashchak_va@fizmat.tnpu.edu.ua>
2014-03-14 15:59:23 +0200

    index action

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -5,13 +5,20 @@ class IndexController extends Zend_Controller_Action
 
     public function init()
     {
+        
         /* Initialize action controller here */
     }
 
-    public function indexAction()
-    {
-        // action body
-    }
+   public function indexAction()
+{
+    // Создаём объект нашей модели
+    $movies = new Application_Model_DbTable_Movies();
+
+    // Применяем метод fetchAll для выборки всех записей из таблицы,
+    // и передаём их в view, через следующую запись
+    $this->view->movies = $movies->fetchAll();
+}
+
 
     public function addAction()
     {
